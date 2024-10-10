fd0ff197d6f07725f6567e4e92a79c84881b447e
ahmadramadhan <ahmadoneperson@gmail.com>
2024-10-10 21:21:00 +0700

    chore: initial routing setup with react router and react query as async state management also fetching library in frontend

---
+++ frontend/src/app/provider.tsx
@@ -0,0 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+
+type AppProviderProps = {
+  children: React.ReactNode
+}
+
+export const AppProvider = ({ children }: AppProviderProps) => {
+  const queryClient = new QueryClient()
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
---
+++ frontend/src/app/router.tsx
@@ -0,0 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+
+const Example = () => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["todos"], queryFn: async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/todos")
+      const todos = await response.json()
+      return todos
+    }
+  })
+
+  if (isLoading) return <h1>Loading ....</h1>
+
+  return (
+    <>
+      <h1>{JSON.stringify(data)}</h1>
+    </>
+  )
+}
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Example />
+  }
+])
+
+export const AppRouter = () => {
+  return <RouterProvider router={router} />
+}
